cb8983d5c07af67d27c6a9abc08316e4f159428a
AkshithBellare <akshithnm@gmail.com>
2020-10-31 18:25:11 +0530

    notebooks

--- dataset_creation.py
+++ dataset_creation.py
@@ -2,20 +2,20 @@ import pandas as pd
 import numpy as np
 import glob 
 
-path_benine="/home/axebell/Desktop/theOne/year3CourseMaterials/se303/project/benign_features.csv" #update the paths here 
-path_malacious="/home/axebell/Desktop/theOne/year3CourseMaterials/se303/project/malicious_features.csv"
+path_benign="/home/axebell/Desktop/theOne/year3CourseMaterials/se303/project/benign_features.csv" #update the paths here 
+path_malicious="/home/axebell/Desktop/theOne/year3CourseMaterials/se303/project/malicious_features.csv"
 
-df_benine =pd.read_csv(path_benine,header=None) 
-df_benine=df_benine.iloc[1:,]
-train_benine, test_benine = np.split(df_benine.sample(frac=1), [int(.75*len(df_benine))]) 
+df_benign =pd.read_csv(path_benine,header=None) 
+df_benign=df_benine.iloc[1:,]
+train_benign, test_benign = np.split(df_benign.sample(frac=1), [int(.75*len(df_benign))]) 
 
-df_malacious =pd.read_csv(path_malacious,header=None) 
-df_malacious=df_malacious.iloc[1:,]
-train_malacious, test_malacious = np.split(df_malacious.sample(frac=1), [int(.75*len(df_malacious))]) 
+df_malicious =pd.read_csv(path_malicious,header=None) 
+df_malicious=df_malicious.iloc[1:,]
+train_malicious, test_malicious = np.split(df_malicious.sample(frac=1), [int(.75*len(df_malicious))]) 
 
 
-train=np.concatenate((train_benine,train_malacious),axis=0)
-test=np.concatenate((test_benine,test_malacious),axis=0)
+train=np.concatenate((train_benign,train_malicious),axis=0)
+test=np.concatenate((test_benign,test_malicious),axis=0)
 
 pd.DataFrame(train).to_csv("train.csv",header=None,index=None)
 pd.DataFrame(test).to_csv("test.csv",header=None,index=None)
